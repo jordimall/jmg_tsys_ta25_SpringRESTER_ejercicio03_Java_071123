f3099fc9d9f469d4501785b094fca009490994bc
jordimall <jordi.mallafre@hotmail.com>
2023-11-07 18:10:40 +0100

    Creación dao

---
+++ src/main/java/com/example/demo/dao/ICajaDAO.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.dto.Caja;
+
+/**
+ * 
+ */
+public interface ICajaDAO extends JpaRepository<Caja, String>{
+
+}
