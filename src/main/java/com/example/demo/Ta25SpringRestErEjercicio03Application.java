e32e39f93a910c78eceb420f314ca264cd6772b2
jordimall <jordi.mallafre@hotmail.com>
2023-11-07 17:42:22 +0100

    First commit

---
+++ src/main/java/com/example/demo/Ta25SpringRestErEjercicio03Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta25SpringRestErEjercicio03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta25SpringRestErEjercicio03Application.class, args);
+	}
+
+}
